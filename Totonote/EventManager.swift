cbda510a05450aafdf5837eb20cb091bc59f0b65
hukko00 <hukkomaccho@outlook.jp>
2026-03-12 21:05:56 +0900

    wow

---
+++ Totonote/EventManager.swift
@@ -0,0 +1,8 @@
+//
+//  EventManager.swift
+//  Totonote
+//
+//  Created by 鈴木勝博 on 2026/03/10.
+//
+
+import Foundation
---
+++ Totonote/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Totonote
+//
+//  Created by 鈴木勝博 on 2026/03/12.
+//
+
+import Foundation
